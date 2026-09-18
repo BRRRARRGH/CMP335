79e87e2faec723d143c3bd7fab05817c4c4fbd8e
BRRRARRGH <thure.w.johnson@gmail.com>
2026-09-18 13:58:39 -0500

    Palindrome finished!

--- Module 2/Palindrome/palindrome.js
+++ Module 2/Palindrome/palindrome.js
@@ -1,12 +1,9 @@
-var List1 = "";
-var List2 = "";
-var List3 = "";
-var Case1 = false;
-var Case2 = false;
-var Case3 = false;
 // ChatGPT prompt: give me 3 different algorithms for solving for a palindrome
 function validateAndAdd() {
-    var ListNum = document.getElementById("whichList").value;
+    let List1 = document.getElementById("List-1-Text").innerHTML;
+    let List2 = document.getElementById("List-2-Text").innerHTML;
+    let List3 = document.getElementById("List-3-Text").innerHTML;
+    let ListNum = document.getElementById("whichList").value;
     if (document.getElementById("input").value == "") { //Validation of input
         alert("Please enter a word to add");
     }
@@ -35,36 +32,36 @@ function validateAndAdd() {
     }
 }
 function isPalindrome1() { //Algorithm 1
-    var word = document.getElementById("input").value; //Gets word to use
-    var myWord = ""; //End word
-    if (!Case1) { //if case sensitivity is off, 
+    let word = document.getElementById("input").value; //Gets word to use
+    let myWord = ""; //End word
+    if (document.getElementById("List-1-Case").innerHTML == "Case Sensitive: False") { //if case sensitivity is off, 
         myWord = word.toLowerCase();
     }
     else {
         myWord = word;
     }
-    var originalArray = myWord.split("");
-    var reversedArray = originalArray.toReversed();
-    for (var i = 0; i < originalArray.length; i++) {
+    let originalArray = myWord.split(""); //creates array and reversed array to compare
+    let reversedArray = originalArray.toReversed();
+    for (let i = 0; i < originalArray.length; i++) { //checks if the arrays are the same
         if (originalArray[i] != reversedArray[i]) {
-            return "False";
+            return "False"; //if not same, not palindrome
         }
     }
-    return "True";
+    return "True"; //if same, is palindrome
 }
-function isPalindrome2() {
-    var word = document.getElementById("input").value;
-    var myWord = "";
-    if (!Case2) {
+function isPalindrome2() { //Algorithm 2
+    let word = document.getElementById("input").value; //gets word
+    let myWord = "";
+    if (document.getElementById("List-2-Case").innerHTML == "Case Sensitive: False") { //Makes case sensitive or not
         myWord = word.toLowerCase();
     }
     else {
         myWord = word;
     }
-    if (myWord.length % 2 == 1) {
-        for (var i = 0, j = myWord.length - 1; i <= ((myWord.length - 1) / 2); i++) {
+    if (myWord.length % 2 == 1) { //If length of word is odd
+        for (let i = 0, j = myWord.length - 1; i <= ((myWord.length - 1) / 2); i++) { //loops through front or back at the same time
             if (myWord[i] != myWord[j]) {
-                return "False";
+                return "False"; //if its not the same, not palindrome
             }
             j--;
             // paaap
@@ -72,10 +69,10 @@ function isPalindrome2() {
             //i = 1, j = 3
             // i = 2, j = 2
         }
-        return "True";
+        return "True"; //if passes, it palindrome
     }
-    else {
-        for (var i = 0, j = myWord.length - 1; i < myWord.length / 2; i++) {
+    else { //if length is even
+        for (let i = 0, j = myWord.length - 1; i < myWord.length / 2; i++) { //Same algo as above
             if (myWord[i] != myWord[j]) {
                 return "False";
             }
@@ -88,10 +85,10 @@ function isPalindrome2() {
         return "True";
     }
 }
-function isPalindrome3() {
-    var word = document.getElementById("input").value;
-    var myWord = "";
-    if (!Case3) {
+function isPalindrome3() { //Algorithm 3
+    let word = document.getElementById("input").value;
+    let myWord = "";
+    if (document.getElementById("List-3-Case").innerHTML == "Case Sensitive: False") { //Makes case sensitive or not
         myWord = word.toLowerCase();
     }
     else {
@@ -99,58 +96,49 @@ function isPalindrome3() {
     }
     return recursioning(myWord);
 }
-function recursioning(myString) {
-    if (myString.length <= 1) {
+function recursioning(myString) {//Recursion function for algorithm 3
+    if (myString.length <= 1) { //if string is one character or less, it is a palindrome
         return "True";
     }
-    if (myString[0] != myString[myString.length - 1]) {
+    if (myString[0] != myString[myString.length - 1]) { //if first and last character are not the same, it is not a palindrome
         return "False";
     }
-    var newWord = myString.slice(1, myString.length - 1);
+    let newWord = myString.slice(1, myString.length - 1); //removes first and last character and calls function again
     return recursioning(newWord);
 }
-function clearList1() {
-    List1 = "";
-    document.getElementById("List-1-Text").innerHTML = List1;
+function clearList1() { //clears list 1
+    document.getElementById("List-1-Text").innerHTML = "";
 }
-function clearList2() {
-    List2 = "";
-    document.getElementById("List-2-Text").innerHTML = List2;
+function clearList2() { // clears list 2
+    document.getElementById("List-2-Text").innerHTML = "";
 }
-function clearList3() {
-    List3 = "";
-    document.getElementById("List-3-Text").innerHTML = List3;
+function clearList3() { // clears list 3
+    document.getElementById("List-3-Text").innerHTML = "";
 }
-function swapCase1() {
+function swapCase1() { //swaps case for list 1
     clearList1();
-    if (Case1) {
-        Case1 = false;
+    if (document.getElementById("List-1-Case").innerHTML == "Case Sensitive: True") {
         document.getElementById("List-1-Case").innerHTML = "Case Sensitive: False";
     }
     else {
-        Case1 = true;
         document.getElementById("List-1-Case").innerHTML = "Case Sensitive: True";
     }
 }
-function swapCase2() {
+function swapCase2() { //swaps case for list 2
     clearList2();
-    if (Case2) {
-        Case2 = false;
+    if (document.getElementById("List-2-Case").innerHTML == "Case Sensitive: True") {
         document.getElementById("List-2-Case").innerHTML = "Case Sensitive: False";
     }
     else {
-        Case2 = true;
         document.getElementById("List-2-Case").innerHTML = "Case Sensitive: True";
     }
 }
-function swapCase3() {
+function swapCase3() { // swaps case for list 3
     clearList3();
-    if (Case3) {
-        Case3 = false;
+    if (document.getElementById("List-3-Case").innerHTML == "Case Sensitive: True") {
         document.getElementById("List-3-Case").innerHTML = "Case Sensitive: False";
     }
     else {
-        Case3 = true;
         document.getElementById("List-3-Case").innerHTML = "Case Sensitive: True";
     }
 }
